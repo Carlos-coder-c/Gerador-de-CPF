10fc36ecbbef309485a5d1040367a923760d62f1
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-06 09:36:27 -0300

    Especificando o cpf

--- gerador.py
+++ gerador.py
@@ -5,13 +5,14 @@ nove_digitos_cpf = ''
 for digitos in range(9):
     nove_digitos_cpf += str(random.randint(0, 9))
 cpf = nove_digitos_cpf
-print(cpf)
+print(cpf, '-> 9 primeiro digitos')
 
 cpf_nove_digitos = cpf[:9] 
 
 contagem_10 = 10
 resultado = 0
 
+<<<<<<< HEAD
 for digito_1 in cpf_nove_digitos:
     resultado += int(digito_1 ) * contagem_10 
     contagem_10 -= 1
@@ -33,8 +34,7 @@ for digito_2 in cpf_dez_digitos:
 digito_2 = (resultado_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
 
-cpf_gerador_final = f"{nove_digitos_cpf}{digito_1}{digito_2}"
+cpf_gerador_final = f"{nove_digitos_cpf}{digito_1}{digito_2} -> CPF COMPLET"
 
 print(cpf_gerador_final)
-
 
